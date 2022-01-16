7e1110e85ab3eb16e76c1e2cc61ae717113aa1af
LetTheBoobsOut <boobs@booby.net>
2022-01-16 19:58:07 +0000

    added regex

--- check.py
+++ check.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import re
 
+#TODO: work on regex , make write status , read from Client
 class CheckStatus():
     def __init__(self):
         self.check_status()
     def check_status(self):
         with open("index.html","r") as f:
             origin = f.read()
-            #TODO: work on regex , make write status , read from Client
-            regex = re.findall(r"^(?\<code>)\w*", origin)
+            regex = re.sub(r"<.*?>",'', origin)
             print(regex)
 
 
